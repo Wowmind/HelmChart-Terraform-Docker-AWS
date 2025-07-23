c0db4cb845aee044da6323ff92e527149fd7f094
wowmind <list>
2025-07-23 13:55:53 +0100

    update

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -12,7 +12,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-yourname"
+    bucket         = "my-bunny-state"
     key            = "eks/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-lock"
